3f003b90df7c6874ab1e98ac7fb7ad8306e66081
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-11-27 07:54:41 +0100

    system_requestable equals informable slot-set

--- plato_based/Ontology.py
+++ plato_based/Ontology.py
@@ -9,3 +9,4 @@ class Ontology:
     def load_ontology(self):
         with open(self.ontology_file_name) as ont_file:
             self.ontology = json.load(ont_file)
+            self.ontology['system_requestable'] = list(self.ontology['informable'].keys())
